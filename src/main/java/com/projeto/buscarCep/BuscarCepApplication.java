fa82afc533ad8a96bc9dc5ba4b8935f3d23c4d35
Otávio Olímpio <otavioolimpio@outlook.com>
2026-02-19 19:50:54 -0300

    Files de Criacao do Projeto

---
+++ src/main/java/com/projeto/buscarCep/BuscarCepApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.buscarCep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscarCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscarCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/buscarCep/BuscarCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.buscarCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscarCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
